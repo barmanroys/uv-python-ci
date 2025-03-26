848a71761324605d679f7b549a3920e7b73340a9
Barman Roy, Swagato <swagatopablo@aol.com>
2025-03-26 10:36:55 +0800

    Docstring added

--- src/yt_uv/__init__.py
+++ src/yt_uv/__init__.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""This file contains the necessary functions to test."""
+
+
+
 def hello() -> str:
+    """A hello function."""
     return "Hello from yt-uv!"
 
 
 def add(a: int, b: int) -> int:
+    """Add two numbers."""
     return a + b
 
 
 def multiply(a: float, b: int) -> float:
+    """Multiply two numbers."""
     return a * b
